2abe6dd0849892535c707a034f62144aac17a165
TiNkoR39 <66479416+TiNkoR39@users.noreply.github.com>
2020-09-12 21:46:22 +0200

    Update First_project_final.py

--- First_project_final.py
+++ First_project_final.py
@@ -8,12 +8,12 @@ def analemma(phi = 45,time_h = 6,time_m = 0):
     
     x0 = 7.53 * np.cos(B) + 1.5 * np.sin(B) - 9.87 * np.sin(2 * B)
     x0 = x0*360/60/24
-    y0 = np.arcsin(np.sin(E*np.pi/180)*np.sin(2*np.pi*(dates-80)/365.2422)) * 180 / np.pi
+    y0 = np.arcsin(np.sin(E*np.pi/180)*np.sin(2*np.pi * (dates-80) / 365.2422)) * 180 / np.pi
     t = time_h + time_m/60
-    alpha = np.cos((t-6) * 2 / 24 * np.pi )*(90-phi)
+    alpha = np.cos((t-6) * 2 / 24 * np.pi )*(90 - phi)
     h = np.sin((t-6)*2/24*np.pi)*(90-phi)
     x = x0 * np.cos(-alpha*np.pi/180) + y0 * np.sin(-alpha*np.pi/180)
-    y = y0 * np.cos(-alpha*np.pi/180) - x0 * np.sin(-alpha*np.pi/180) +h
+    y = y0 * np.cos(-alpha*np.pi/180) - x0 * np.sin(-alpha*np.pi/180)  +h
 
     
     plt.axis('equal')
